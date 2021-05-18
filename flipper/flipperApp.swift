889243d2e1dda26dd3d427ed89cf344ca1e5ab06
kenta.takano <san.kun.sakana@gmail.com>
2021-05-18 23:31:29 +0900

    Initial Commit

---
+++ flipper/flipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  flipperApp.swift
+//  flipper
+//
+//  Created by ohayoukenchan on 2021/05/18.
+//
+
+import SwiftUI
+
+@main
+struct flipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
